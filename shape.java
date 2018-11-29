63b12cea00d2475fd55df0b772122779ac30d39f
techrahuloff <45458165+techrahuloff@users.noreply.github.com>
2018-11-29 18:36:11 +0530

    converted to sphere

--- shape.java
+++ shape.java
@@ -4,8 +4,9 @@ public class shape
 public static void main (string[]args)
 {
 Scanner sc =new Scanner(System.in);
-System.out.println("Enter the radius of circle to calculate area of the circle");
+System.out.println("Enter the radius of sphere to calculate area of the circle");
 int r=sc.nextInt();
-System.out.println("area of circle is"+r*r);
+System.out.println("area of sphere is"+r*r*r);
 }}
 */to create a bversion 2 in this file  
+*/ now to creating a branch so making a change in code 
