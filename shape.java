ae9fe746315614c2dded0e39039c2ce292d3921f
techrahuloff <45458165+techrahuloff@users.noreply.github.com>
2018-11-29 18:31:49 +0530

    only area

--- shape.java
+++ shape.java
@@ -3,7 +3,6 @@ public class shape
 {
 public static void main (string[]args)
 {
-System.out.println("the shape of circle is a diameter of radius 1");
 Scanner sc =new Scanner(System.in);
 System.out.println("Enter the radius of circle to calculate area of the circle");
 int r=sc.nextInt();
