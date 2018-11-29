e6c404f347c86957e90130093f51d0b1e1ab11c6
techrahuloff <45458165+techrahuloff@users.noreply.github.com>
2018-11-29 18:30:27 +0530

    create shape

---
+++ shape.java
@@ -0,0 +1,11 @@
+import.java.util.io*
+public class shape
+{
+public static void main (string[]args)
+{
+System.out.println("the shape of circle is a diameter of radius 1");
+Scanner sc =new Scanner(System.in);
+System.out.println("Enter the radius of circle to calculate area of the circle");
+int r=sc.nextInt();
+System.out.println("area of circle is"+r*r);
+}}
